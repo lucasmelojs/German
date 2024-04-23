988ba9723b4f582ebefef89a26faba48b6f4b204
lucasmelojs <lucascunhamelo@gmail.com>
2024-04-23 20:51:50 -0300

    deploy

--- Captura/src/routes/Zotrim.jsx
+++ Captura/src/routes/Zotrim.jsx
@@ -5,7 +5,7 @@ const Zotrim = () => {
         <>
             <header>
                 <div className={styles.header}>
-                    <a href="https://theikariajuice.com/?shield=f8041bnzx2yysu8bg41fd-1za3">
+                    <a href="https://zotrim.com/?_ef_transaction_id=&oid=13&affid=37993">
                         <img className={styles.logo} src="zotrim-logo.png" alt="logo" />
                     </a>
                 </div>
@@ -13,24 +13,71 @@ const Zotrim = () => {
             <section className={styles.hero_container}>
                 <div className={styles.hero_wrapper}>
                     <h1>Zotrim, The Clinically Proven Appetite Suppressant</h1>
-                    <button className={styles.hero_button}>ORDER NOW</button>
+                    <a href='https://zotrim.com/?_ef_transaction_id=&oid=13&affid=37993'>
+                        <button className={styles.hero_button}>ORDER NOW</button>
+                    </a>
                 </div>
-                <img className={styles.hero_photo} src="loose_weight.png" alt="logo" />
+                <div>
+                    <picture className={styles.hero_photo}>
+                        <source media="(max-width: 440px)" srcSet="weight_small.png" />
+                        <source media="(max-width: 768px)" srcSet="weight_medium.png" />
+                        <img src="weight_large.png" alt="lost-weight" />
+                    </picture>
+                </div>
             </section>
             <section className={styles.pros_container}>
-                <img src="eat-less.png" alt="eat-less" />
-                <img src="studies-papers.png" alt="studies-papers" />
-                <img src="experts-approved.png" alt="experts-approve" />
-                <img src="fuller-for-longer.png" alt="fuller-for-longe" />
+                <picture>
+                    <source media="(max-width: 440px)" srcSet="experts-small.png" />
+                    <source media="(max-width: 768px)" srcSet="experts-medium.jpg" />
+                    <img src="experts-large.jpg" alt="experts-approve" />
+                </picture>
+                <picture>
+                    <source media="(max-width: 440px)" srcSet="studies-small.png" />
+                    <source media="(max-width: 768px)" srcSet="studies-medium.jpg" />
+                    <img src="studies-large.jpg" alt="clinical-studies" />
+                </picture>
+                <picture>
+                    <source media="(max-width: 440px)" srcSet="eat-less-small.png" />
+                    <source media="(max-width: 768px)" srcSet="eat-less-medium.jpg" />
+                    <img src="eat-less-large.jpg" alt="eat-less" />
+                </picture>
+                <picture>
+                    <source media="(max-width: 440px)" srcSet="fuller-small.png" />
+                    <source media="(max-width: 768px)" srcSet="fuller-medium.jpg" />
+                    <img src="fuller-large.jpg" alt="fuller-longer" />
+                </picture>
             </section>
             <hr />
             <section className={styles.testimony_container}>
                 <h1>Success Stories</h1>
                 <div className={styles.testimony_wrapper}>
-                    <img src="smith.jpg" alt="testimony-1" />
-                    <img src="florence.jpg" alt="testimony-2" />
-                    <img src="michelle.jpg" alt="testimony-3" />
-                    <img src="crellin.jpg" alt="testimony-4" />
+                    <picture>
+                        <source media="(max-width: 440px)" srcSet="smith-small.jpg" />
+                        <source media="(max-width: 768px)" srcSet="smith-large.jpg" />
+                        <img src="smith-large.jpg" alt="testimony-1" />
+                    </picture>
+                    <picture>
+                        <source media="(max-width: 440px)" srcSet="florence-small.jpg" />
+                        <source media="(max-width: 768px)" srcSet="florence-large.jpg" />
+                        <img src="florence-large.jpg" alt="testimony-2" />
+                    </picture>
+                    <picture>
+                        <source media="(max-width: 440px)" srcSet="michelle-small.jpg" />
+                        <source media="(max-width: 768px)" srcSet="michelle-large.jpg" />
+                        <img src="michelle-large.jpg" alt="testimony-3" />
+                    </picture>
+                    <picture>
+                        <source media="(max-width: 440px)" srcSet="crellin-small.jpg" />
+                        <source media="(max-width: 768px)" srcSet="crellin-large.jpg" />
+                        <img src="crellin-large.jpg" alt="testimony-4" />
+                    </picture>
+
+                </div>
+
+                <div className={styles.testimony_button_container}>
+                    <a href='https://zotrim.com/?_ef_transaction_id=&oid=13&affid=37993'>
+                        <button className={styles.testimony_button}>ORDER NOW</button>
+                    </a>
                 </div>
             </section>
         </>
